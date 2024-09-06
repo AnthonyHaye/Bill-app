5d3edc0cd40989684315729a3bee9cb1fa88db78
Anthony Hayé <anthony.haye49@laposte.net>
2024-09-06 15:32:56 +0200

    Correction du test de login correction du fichier container/login.js const user Admin, les inputs de querySelector étaient avec employee. Correction également du fichier NewBill, pour accepté uniquement les fichiers avec les extention attendu

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -16,10 +16,21 @@ export default class NewBill {
     new Logout({ document, localStorage, onNavigate })
   }
   handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    e.preventDefault();
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`);
+    const file = fileInput.files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+
+    const allowedExtensions = ['jpg', 'jpeg', 'png'];
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+
+    if (!allowedExtensions.includes(fileExtension)) {
+      alert('Seules les images de type jpg, jpeg ou png sont autorisées.');
+      fileInput.value = ''; 
+      return;
+  }
+
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
