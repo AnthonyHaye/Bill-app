497ba6c0d666cd340f8bcaf2fef62d629cb36a5f
Anthony Hayé <anthony.haye49@laposte.net>
2024-09-10 11:49:00 +0200

    ajout du test pour vérifier que la fonction 'handleclickNewBill'  redirige bien vers la page de création d'une nouvelle facture lorsque l'utilisateur clique sur le bouton

--- Front/src/__tests__/Bills.js
+++ Front/src/__tests__/Bills.js
@@ -9,6 +9,8 @@ import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -31,19 +33,26 @@ describe("Given I am connected as an employee", () => {
     })
     
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      
-      // Récupération des dates
+      document.body.innerHTML = BillsUI({ data: bills })    
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      console.log("Dates trouvées :", dates)  // Affichage des dates récupérées
-      
-      // Tri anti-chronologique
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
-      console.log("Dates triées :", datesSorted)  // Affichage des dates après tri
-      
-      // Vérification de l'ordre
+      console.log("Dates triées :", datesSorted)  
       expect(dates).toEqual(datesSorted)
     })
   })
+  describe("When I click on the 'New Bill' button" ,() => {
+    test("Then it should navigate to the NewBill page", () => {
+      const onNavigate = jest.fn()
+      console.log (onNavigate)
+      const billsContainer = new Bills({ document, onNavigate, store: null, localStorage: window.localStorage})
+      const buttonNewBill = screen.getByTestId("btn-new-bill")
+      buttonNewBill.addEventListener('click', billsContainer.handleClickNewBill)
+      userEvent.click(buttonNewBill)
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH['NewBill'])
+    })
+  })
+
+
+
 })
